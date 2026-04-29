379f162a61e0dd4ee50d57132e34592da8bdfff4
Dona121 <carrascal544@gmail.com>
2026-04-28 20:18:03 -0500

    Base de datos postgresql local

--- convocatorias_seguimiento/convocatorias_seguimiento/settings.py
+++ convocatorias_seguimiento/convocatorias_seguimiento/settings.py
@@ -96,24 +96,17 @@ WSGI_APPLICATION = 'convocatorias_seguimiento.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True
-            ),
+DATABASES = {
+    'default': {
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT"),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
